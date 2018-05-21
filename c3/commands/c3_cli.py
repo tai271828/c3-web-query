014422623d99a43e77741a0dbdf65277d61f7371
Taihsiang Ho (tai271828) <tai271828@gmail.com>
2018-05-21 22:14:33 +0800

    PEP8 fix

--- c3/commands/c3_cli.py
+++ c3/commands/c3_cli.py
@@ -3,23 +3,19 @@ import click
 from c3.commands.pool import commands as group_batch
 from c3.commands.single import commands as group_single
 
+
 @click.group()
 @click.option('--c3username',
-              default=lambda: os.environ.get('C3USERNAME',''),
+              default=lambda: os.environ.get('C3USERNAME', ''),
               help='Your C3 username')
 @click.option('--c3apikey',
-              default=lambda: os.environ.get('C3APIKEY',''),
+              default=lambda: os.environ.get('C3APIKEY', ''),
               help='Your C3 API KEY')
 @click.option('--verbose', default=1,
               help='Verbose level, the lower the fewer message output.')
 @click.option('--level', default=1,
               help='Review level, the lower number means lower pass threshold.')
-@click.option('--report',
-              help='Render the results including customized notes of users.')
-@click.option('--silent',
-              help='The silent mode is useful for automation.')
-
-def main(c3username, c3apikey, verbose, level, report, silent):
+def main(c3username, c3apikey, verbose, level):
     pass
 
 
