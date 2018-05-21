c25f99be6f1ada68a103049f58219e37fee4f5d2
Taihsiang Ho (tai271828) <tai271828@gmail.com>
2018-05-21 22:26:35 +0800

    Add more component options

--- c3/commands/pool/commands.py
+++ c3/commands/pool/commands.py
@@ -2,11 +2,14 @@ import click
 
 
 @click.command()
-@click.argument('algorithm')
+@click.option('--component',
+              type=click.Choice(['cpu', 'wifi', 'dkms', 'all']),
+              default='all',
+              help='The top concern of diversity of the pool.')
 @click.option('--certificate',
               type=click.Choice(['14.04.5', '16.04']),
               default='16.04',
-              help='Filters to match certify status')
+              help='Filters to match certify status.')
 def create(algorithm, certificate):
     """
     Create a test pool by given categories.
