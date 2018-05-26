b6d0c03a6eb4c24de8c720aadf736402beb4c609
Taihsiang Ho (tai271828) <tai271828@gmail.com>
2018-05-26 10:22:17 +0800

    Add more test cases for default.ini

--- test/test_config.py
+++ test/test_config.py
@@ -4,22 +4,32 @@ Test configuration parser
 Usage: project_root> pytest
 
 """
-import c3.config
-import pkg_resources
-
-resource_package = __name__
-resource_path = '/'.join(('../c3/data', 'default.ini'))
-
-template = pkg_resources.resource_stream(resource_package, resource_path)
+import pytest
 
 
-def read_default():
+@pytest.fixture
+def config_singlet():
+    """
+    Read the default.ini and return config singlet.
+
+    :return: configuration singlet instance.
+    """
+    import pkg_resources
+    import c3.config
+
+    resource_package = __name__
+    resource_path = '/'.join(('../c3/data', 'default.ini'))
+
+    template = pkg_resources.resource_stream(resource_package, resource_path)
+
     config_singlet = c3.config.Configuration()
     config_singlet.read_configuration(template.name)
     return config_singlet
 
 
-def test_read_default():
-    config_singlet = read_default()
-
+def test_read_default_username(config_singlet):
     assert config_singlet.config['C3']['UserName'] == 'ubuntu'
+
+
+def test_read_default_apikey(config_singlet):
+    assert config_singlet.config['C3']['APIKey'] == 'ubuntu'
