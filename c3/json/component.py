1899363708adf20ee9bee137e4bfb8073cc80dee
Taihsiang Ho (tai271828) <tai271828@gmail.com>
2018-05-28 00:30:18 +0800

    Modulize with more shared lookup tables

--- c3/json/component.py
+++ c3/json/component.py
@@ -2,21 +2,23 @@
 Convert C3 response in json to basic target information.
 """
 import logging
+from c3.maptable import machine_metainfo_attr as mma
+from c3.maptable import device_audio_attr as daa
 
 
 logger = logging.getLogger('c3_web_query')
 
 
 def get_audio_component(device_report):
-    rtn_device = {'audio_pciid': 'Unknown pciid',
-                  'audio_name': 'Audio is not found'}
+    rtn_device = {daa[0]: 'Unknown pciid',
+                  daa[1]: 'Audio is not found'}
 
     for device in device_report:
         if device['bus'] == 'pci':
             try:
                 if device['category']['name'] == 'AUDIO':
-                    rtn_device['audio_pciid'] = device['identifier']
-                    rtn_device['audio_name'] = device['name']
+                    rtn_device[daa[0]] = device['identifier']
+                    rtn_device[daa[1]] = device['name']
                     return rtn_device
             except Exception as e:
                 logger.warning(e)
@@ -32,8 +34,7 @@ def get_machine_info(machine_report):
     :return: a dictionary to fill the report list
     """
     rtn_dict = {}
-    keys = ['make', 'model', 'codename', 'form_factor', 'processor',
-            'video', 'wireless', 'network']
+    keys = mma
 
     # If there is no submission, machine_report is None
     if machine_report is None:
--- c3/maptable.py
+++ c3/maptable.py
@@ -8,3 +8,9 @@ location = {'beijing': '6', 'ceqa': '12', 'taipei': '13'}
 loglevel = {'debug': logging.DEBUG, 'info': logging.INFO,
             'warning': logging.WARNING, 'error': logging.ERROR,
             'critical': logging.CRITICAL}
+
+
+machine_metainfo_attr = ['make', 'model', 'codename', 'form_factor',
+                         'processor', 'video', 'wireless', 'network']
+
+device_audio_attr = ['audio_pciid', 'audio_name']
