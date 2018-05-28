cfb0905d1ac3049640dadfba1ca7bf139a251cb0
Taihsiang Ho (tai271828) <tai271828@gmail.com>
2018-05-29 00:31:28 +0800

    Fix shrink algorithm bug

--- c3/shrink/shrink.py
+++ c3/shrink/shrink.py
@@ -9,11 +9,12 @@ logger = logging.getLogger('c3_web_query')
 def sanity_check(base, target):
     for key in base:
         if key not in target:
+            logger.critical("Different device number.")
             raise Exception
 
     if len(base) != len(target):
+        logger.critical("Different device number.")
         raise Exception
-
 
 def get_unique_devices_in_pool(cid_objs, category, ifamily=False):
     ifamily_series = c3.maptable.ifamily_series
@@ -23,10 +24,7 @@ def get_unique_devices_in_pool(cid_objs, category, ifamily=False):
         device = getattr(cid_obj, category)
 
         # group i[3,5,7]-xxxx as i[3,5,7]
-        if ifamily and category == 'processor':
-            for ifamily_type in ifamily_series:
-                if ifamily_type in device.lower():
-                    device = ifamily_type
+        device = get_group_cpu_name(ifamily, category, device)
 
         if device not in devices_in_pool:
             devices_in_pool.append(device)
@@ -36,39 +34,57 @@ def get_unique_devices_in_pool(cid_objs, category, ifamily=False):
             device_counts_in_pool[device] = 1
 
     sanity_check(devices_in_pool, device_counts_in_pool.keys())
+    logger.info('Sanity check completed. %s looks good.' % category)
 
     unique_devices_in_pool = []
+    duplicate_devices_in_pool = []
     for device in device_counts_in_pool.keys():
         if device_counts_in_pool[device] == 1:
             unique_devices_in_pool.append(device)
+        else:
+            duplicate_devices_in_pool.append(device)
 
-    return unique_devices_in_pool
+    return unique_devices_in_pool, duplicate_devices_in_pool
+
+
+def get_group_cpu_name(ifamily, category, device):
+    ifamily_series = c3.maptable.ifamily_series
+    if ifamily and category == 'processor':
+        for ifamily_type in ifamily_series:
+            if ifamily_type in device.lower():
+                device = ifamily_type
+
+    return device
 
 
 def shrink_by_category(cid_objs, device_categories, ifamily=False):
     # Find unique devices per category
     category_unique_devices = {}
+    category_duplicate_devices = {}
     for category in device_categories:
-        unique_d_pool = get_unique_devices_in_pool(cid_objs, category, ifamily)
+        unique_d_pool, duplicate_d_pool = get_unique_devices_in_pool(
+            cid_objs, category, ifamily)
         category_unique_devices[category] = unique_d_pool
+        category_duplicate_devices[category] = duplicate_d_pool
 
     all_unique_devices = []
+    all_duplicate_devices = []
     for category in category_unique_devices.keys():
         all_unique_devices.extend(category_unique_devices[category])
+        all_duplicate_devices.extend(category_duplicate_devices[category])
+    logger.info("All unique devices: %s" % all_unique_devices)
 
+    print('Before shrink: %i CIDs in the pool' % len(cid_objs))
     # Begin to shrink
+    # phase 1: pick up those very unique
     cid_objs_shrunk = []
-    ifamily_series = c3.maptable.ifamily_series
     for cid_obj in cid_objs:
         flag_pickup = False
         for category in device_categories:
             logging.debug("Shrink the pool by %s" % category)
             device = getattr(cid_obj, category)
 
-            if ifamily and category == 'processor':
-                for ifamily_type in ifamily_series:
-                    if ifamily_type in device.lower():
-                        device = ifamily_type
+            device = get_group_cpu_name(ifamily, category, device)
 
             if device in all_unique_devices:
                 flag_pickup = True
@@ -76,7 +92,35 @@ def shrink_by_category(cid_objs, device_categories, ifamily=False):
         if flag_pickup:
             cid_objs_shrunk.append(cid_obj)
 
-    print('Before shrink: %i CIDs in the pool' % len(cid_objs))
+    # phase 2: check which duplicate components were already picked up in
+    # the systems with unique devices.
+    for cid_obj in cid_objs_shrunk:
+        for device in list(all_duplicate_devices):
+            for category in device_categories:
+                device_cid = getattr(cid_obj, category)
+                device_cid = get_group_cpu_name(ifamily, category, device_cid)
+                if device == device_cid:
+                    all_duplicate_devices.remove(device)
+
+    # phase 3: pick up systems based on the duplicate devices.
+    # This phase is where we could set up the priority of location etc. when
+    # picking up systems.
+    for cid_obj in cid_objs_shrunk:
+        cid_objs.remove(cid_obj)
+
+    for cid_obj in cid_objs:
+        flag_pickup = False
+        for device in list(all_duplicate_devices):
+            for category in device_categories:
+                device_cid = getattr(cid_obj, category)
+                device_cid = get_group_cpu_name(ifamily, category, device_cid)
+                if device == device_cid:
+                    all_duplicate_devices.remove(device)
+                    flag_pickup = True
+
+        if flag_pickup:
+            cid_objs_shrunk.append(cid_obj)
+
     print('After shrink:  %i CIDs in the pool' % len(cid_objs_shrunk))
 
     return cid_objs_shrunk
