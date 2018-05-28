9fee8225241af13d1201dc2aa28f8df68e9175d1
Taihsiang Ho (tai271828) <taihsiang.ho@canonical.com>
2018-05-28 14:20:59 +0800

    include mainstream into location candidates

--- c3/maptable.py
+++ c3/maptable.py
@@ -3,7 +3,7 @@ All lookup tables.
 """
 import logging
 
-location = {'beijing': '6', 'ceqa': '12', 'taipei': '13'}
+location = {'beijing': '6', 'mainstream': '10', 'ceqa': '12', 'taipei': '13'}
 
 loglevel = {'debug': logging.DEBUG, 'info': logging.INFO,
             'warning': logging.WARNING, 'error': logging.ERROR,
